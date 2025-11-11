d5b832a694640b30880233b48107cd8ea27a0347
DomaniqueK <domaniqueking24@gmail.com>
2025-11-11 12:08:24 -0600

    Created the generateTime method and it flows, Updated the pizza class with a constructor that takes crust type and size

--- src/main/java/Pizza.java
+++ src/main/java/Pizza.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class Pizza {
+    private int eight = 8;
+    private int twelve = 12;
+    private int sixteen = 16;
     private String thin;
     private String regular;
     private String thick;
@@ -24,196 +27,13 @@ public class Pizza {
     private String basil;
     private String olives;
     private String marinara;
-
-    public String getMarinara() {
-        return marinara;
-    }
-
-    public void setMarinara(String marinara) {
-        this.marinara = marinara;
-    }
-
-    public String getAlfredo() {
-        return alfredo;
-    }
-
-    public void setAlfredo(String alfredo) {
-        this.alfredo = alfredo;
-    }
-
-    public String getPesto() {
-        return pesto;
-    }
-
-    public void setPesto(String pesto) {
-        this.pesto = pesto;
-    }
-
-    public String getBbq() {
-        return bbq;
-    }
-
-    public void setBbq(String bbq) {
-        this.bbq = bbq;
-    }
-
-    public String getBuffalo() {
-        return buffalo;
-    }
-
-    public void setBuffalo(String buffalo) {
-        this.buffalo = buffalo;
-    }
-
     private String alfredo;
     private String pesto;
     private String bbq;
     private String buffalo;
     private ArrayList<Pizza> pizza = new ArrayList<>();
 
-    public String getPepperoni() {
-        return pepperoni;
-    }
 
-    public void setPepperoni(String pepperoni) {
-        this.pepperoni = pepperoni;
-    }
-
-    public String getSausage() {
-        return sausage;
-    }
-
-    public void setSausage(String sausage) {
-        this.sausage = sausage;
-    }
-
-    public String getHam() {
-        return ham;
-    }
-
-    public void setHam(String ham) {
-        this.ham = ham;
-    }
-
-    public String getBacon() {
-        return bacon;
-    }
-
-    public void setBacon(String bacon) {
-        this.bacon = bacon;
-    }
-
-    public String getChicken() {
-        return chicken;
-    }
-
-    public void setChicken(String chicken) {
-        this.chicken = chicken;
-    }
-
-    public String getMeatball() {
-        return meatball;
-    }
-
-    public void setMeatball(String meatball) {
-        this.meatball = meatball;
-    }
-
-    public String getExtraMeat() {
-        return extraMeat;
-    }
-
-    public void setExtraMeat(String extraMeat) {
-        this.extraMeat = extraMeat;
-    }
-
-    public String getMozzarella() {
-        return mozzarella;
-    }
-
-    public void setMozzarella(String mozzarella) {
-        this.mozzarella = mozzarella;
-    }
-
-    public String getParmesan() {
-        return parmesan;
-    }
-
-    public void setParmesan(String parmesan) {
-        this.parmesan = parmesan;
-    }
-
-    public String getRicotta() {
-        return ricotta;
-    }
-
-    public void setRicotta(String ricotta) {
-        this.ricotta = ricotta;
-    }
-
-    public String getExtraCheese() {
-        return extraCheese;
-    }
-
-    public void setExtraCheese(String extraCheese) {
-        this.extraCheese = extraCheese;
-    }
-
-    public String getOnions() {
-        return onions;
-    }
-
-    public void setOnions(String onions) {
-        this.onions = onions;
-    }
-
-    public String getBellPeppers() {
-        return bellPeppers;
-    }
-
-    public void setBellPeppers(String bellPeppers) {
-        this.bellPeppers = bellPeppers;
-    }
-
-    public String getPineapples() {
-        return pineapples;
-    }
-
-    public void setPineapples(String pineapples) {
-        this.pineapples = pineapples;
-    }
-
-    public String getSpinach() {
-        return spinach;
-    }
-
-    public void setSpinach(String spinach) {
-        this.spinach = spinach;
-    }
-
-    public String getBasil() {
-        return basil;
-    }
-
-    public void setBasil(String basil) {
-        this.basil = basil;
-    }
-
-    public String getOlives() {
-        return olives;
-    }
-
-    public void setOlives(String olives) {
-        this.olives = olives;
-    }
-
-    public ArrayList<Pizza> getPizza() {
-        return pizza;
-    }
-
-    public void setPizza(ArrayList<Pizza> pizza) {
-        this.pizza = pizza;
-    }
 
     public String getThin() {
         return thin;
@@ -255,22 +75,18 @@ public class Pizza {
         this.cauliflower = cauliflower;
     }
 
-    public ArrayList<Pizza> getPizzaCrust() {
-        return pizza;
-    }
 
-    public void setPizzaCrust(ArrayList<Pizza> pizza) {
-        this.pizza = pizza;
-    }
-
-
-    public Pizza() {
+    public Pizza(int eight, int twelve, int sixteen, String thin, String thick, String regular, String stuffed, String cauliflower) {
         this.thin = thin;
         this.regular = regular;
         this.thick = thick;
         this.stuffed = stuffed;
         this.cauliflower = cauliflower;
-        this.pizza = pizza;
+        this.eight = eight;
+        this.twelve = twelve;
+        this.sixteen = sixteen;
+    }
+ /*       this.pizza = pizza;
         this.pepperoni = pepperoni;
         this.sausage = sausage;
         this.ham = ham;
@@ -293,7 +109,7 @@ public class Pizza {
         this.pesto = pesto;
         this.bbq = bbq;
         this.buffalo = buffalo;
+*/
 
-    }
 
 }
--- src/main/java/Receipt.java
+++ src/main/java/Receipt.java
@@ -1,8 +1,11 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class Receipt {
     public static void main(String[] args) {
@@ -10,10 +13,11 @@ public class Receipt {
         generateTime();
     }
 
-    public static void  saveReceipt() {
+    public static void  saveReceipt(OrderScreen orderScreen) {
         try {
-            FileWriter fileWriter = new FileWriter("src/main/resources/Receipts");
+            FileWriter fileWriter = new FileWriter("src/main/resources/receipt.txt");
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            fileWriter.write("Receipt: \n");
 
         } catch (IOException e) {
             System.out.println("Error! Please try again!" + e);
@@ -21,6 +25,9 @@ public class Receipt {
     }
 
     public static void generateTime() {
-        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy HH:mm");
+        LocalDateTime date = LocalDateTime.now();
+        System.out.println(date.format(formatter));
+
     }
 }
